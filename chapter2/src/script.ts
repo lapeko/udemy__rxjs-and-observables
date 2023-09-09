6ee7237bd4bcbe8d0b602e28c234937eed0194e9
lapeko <lapeko88@gmail.com>
2023-09-09 18:02:10 +0200

    extend the example

--- chapter2/src/script.ts
+++ chapter2/src/script.ts
@@ -1,4 +1,4 @@
-import { Observable } from "rxjs";
+import { Observable, Observer } from "rxjs";
 
 const observable$ = new Observable<string>((subscriber) => {
   subscriber.next("Anya");
@@ -6,8 +6,14 @@ const observable$ = new Observable<string>((subscriber) => {
   setTimeout(() => subscriber.next("Sveta"), 4000);
 });
 
-const observer1 = (value: string): void => console.log("subscriber 1,", value);
-const observer2 = (value: string): void => console.log("subscriber 2", value);
+const observer1: Observer<string> = {
+  next(value) {
+    console.log("observer 1", value);
+  },
+  error() {},
+  complete() {},
+};
+const observer2 = (value: string): void => console.log("observer 2 2", value);
 
 observable$.subscribe(observer1);
 setTimeout(() => observable$.subscribe(observer2), 1000);
