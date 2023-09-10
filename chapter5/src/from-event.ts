2f5cb76681d9d5dc91ace8263b4ab3bcdb1da9b5
lapeko <lapeko88@gmail.com>
2023-09-10 15:31:07 +0200

    learn fromEvent creation function

---
+++ chapter5/src/from-event.ts
@@ -0,0 +1,29 @@
+import Emitter from "events";
+import { Observable } from "rxjs";
+// import { fromEvent } from "rxjs";
+
+const emitter = new Emitter();
+
+let i = 0;
+setInterval(() => emitter.emit("tick", ++i), 1000);
+
+// fromEvent(emitter, "tick").subscribe({ next: (value) => console.log(value) });
+
+function fromEvent(target: Emitter, name: string) {
+  return new Observable((subscriber) => {
+    const handler = (value: any) => {
+      console.log("handler");
+      subscriber.next(value);
+    };
+    emitter.on(name, handler);
+    return () => emitter.removeListener(name, handler);
+  });
+}
+
+const subscription$ = fromEvent(emitter, "tick").subscribe((value) =>
+  console.log(value)
+);
+setTimeout(() => {
+  console.log("Unsubscribe");
+  subscription$.unsubscribe();
+}, 5000);
